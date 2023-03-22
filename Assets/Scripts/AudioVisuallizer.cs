555e5069719b8e1ca848436be0bd4a0a99ff0765
nicemusk <105399890+nicemusk@users.noreply.github.com>
2023-03-22 22:51:30 +0900

    Man on Load

--- Assets/Scripts/AudioVisuallizer.cs
+++ Assets/Scripts/AudioVisuallizer.cs
@@ -7,14 +7,43 @@ using UnityEngine;
 
 public class AudioVisuallizer : MonoBehaviour
 {
-    public AudioSource _audioSource;
 
-    public float[] _samples = new float[255];
+    [Space(15)]
+    public AudioClip audioClip;
+    public bool loop = true;
+
+    public AudioSource _audioSource;
+    [Space(15), Range(64, 8192)]
+    public int visualizerSimples = 64;
+    [Space(15), Range(0.0f, 1.0f)]
+    public float _volume = 1.0f;
+    [Space(15), Range(0.0f, 1.0f)]
+    public float Sensitive = 0.0f;
+    [Space(15), Range(0, 100)]
+    public int _intensity = 1;
+
+    public Light spotlight;
+
+    AudioSource audioSource;
+    Rigidbody cam;
+    Transform _cam;
+    public int force;
+
+    private void Awake()
+    {
+        cam = GetComponent<Rigidbody>();
+        _cam = GetComponent<Transform>();
+        //spotlight = GetComponentInChildren<Light>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         //_audioSource = GetComponent<AudioSource>();
+        audioSource = new GameObject("_AudioSource").AddComponent<AudioSource>();
+        audioSource.loop = loop;
+        audioSource.clip = audioClip;
+        audioSource.Play();
     }
 
     // Update is called once per frame
@@ -26,6 +55,24 @@ public class AudioVisuallizer : MonoBehaviour
 
     void GetSpectrumAudioSource()
     {
-        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman); 
+        float[] spectrumData = audioSource.GetSpectrumData(visualizerSimples, 0, FFTWindow.Rectangular);
+        for (int i = 0; i < spectrumData.Length; i++)
+        {
+            //_cam.Translate(Vector3.forward * _intensity * Time.deltaTime);
+            if (spectrumData[i] > 0.2)
+                {
+                cam.AddForce(Vector3.right * spectrumData[i] * force * 2 * Time.deltaTime);
+                //_cam.Translate(Vector3.forward * spectrumData[i] * force * Time.deltaTime);
+                //spotlight.intensity = 1 * _intensity;
+            }
+                else
+                {
+                cam.AddForce(Vector3.right * spectrumData[i] * force * Time.deltaTime);
+                //_cam.Translate(Vector3.forward * spectrumData[i] * Time.deltaTime);
+                //spotlight.intensity = 1;
+                }
+            spotlight.intensity = spectrumData[i] * _intensity;
+            
+        }
     }
 }
